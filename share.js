0c26e0efa5d1eb52670c345cb06fe47e32a78644
James King <allthepie@gmail.com>
2020-06-14 16:26:11 -0400

    List fixed-width images with hyperlink and modification time.

--- share.js
+++ share.js
@@ -1,4 +1,8 @@
 var share_bucket = "yevano-file-share";
+var share_path = "share";
+
+var s3_url = "https://s3.amazonaws.com";
+var share_url = `${ s3_url }/${ share_bucket }`;
 
 AWS.config.region = "us-east-1";
 
@@ -11,22 +15,40 @@ var s3_handle = new AWS.S3({
     params: { Bucket: share_bucket }
 });
 
-window.addEventListener("load", event => {
+window.addEventListener("load", _ => {
     var file_list_element = document.getElementById("file-list");
     
     s3_handle.listObjects({
         Bucket: share_bucket
-    }, (err, data) => {
-        //file_list_element.appendChild(document.createTextNode(`${ JSON.stringify(data) }`));
-        
+    }, (err, data) => {        
         if(err) {
             alert(`Could not list share files. Error: ${ err.message }`);
         } else {
             for(let content of data.Contents) {
-                for(let [key, value] of Object.entries(content)) {
-                    file_list_element.appendChild(document.createTextNode(`${ key }: ${ value }`));
-                    file_list_element.appendChild(document.createElement("br"));
-                }
+                var file_url = `${ share_url }/${ content.Key }`
+
+                var file_img_element = document.createElement("img");
+                file_img_element.src = file_url;
+                file_img_element.style = "width: 400px";
+
+                var img_div = document.createElement("div");
+                img_div.className = "file-item";
+                img_div.appendChild(file_img_element);
+
+                var text = `${ content.Key.substr(file_url.indexOf("/")) }`;
+                
+                var info_anchor = document.createElement("a");
+                info_anchor.href = file_url;
+                info_anchor.appendChild(document.createTextNode(text));
+
+                var info_div = document.createElement("div");
+                info_div.className = "file-item";
+                info_div.appendChild(info_anchor);
+                info_div.appendChild(document.createElement("br"));
+                info_div.appendChild(document.createTextNode(content.LastModified));
+
+                file_list_element.appendChild(img_div);
+                file_list_element.appendChild(info_div);
             }
         }
     })
